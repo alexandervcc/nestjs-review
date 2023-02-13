c17f737417b5d47f3ba9b62cba72aa967aa139a7
Alex Charco Cuenca <alex.charco.25@gmail.com>
2023-02-13 15:02:08 -0500

    Basic Controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Req,
+  Post,
+  Body,
+  Header,
+  Redirect,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
+import { Dog } from './model/Dog';
 
-@Controller()
+@Controller('dogs')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @HttpCode(200)
+  @Header('Cache-Control', 'none')
+  getHello(@Req() request: Request): Dog[] {
+    return this.appService.getAllDogs();
   }
+
+  @Post()
+  @HttpCode(201)
+  createDog(@Body() dogDto: Dog): Dog {
+    return this.appService.createNewDog(dogDto);
+  }
+
+  @Get("/redirect")
+  @Redirect('https://nestjs.com', 301)
+  redirectTest() {}
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Dog } from './model/Dog';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private counter = 0;
+  private listDogs: Dog[] = [];
+
+  getAllDogs(): Dog[] {
+    return this.listDogs.slice();
+  }
+
+  createNewDog(newDog: Dog): Dog {
+    newDog.id = this.counter;
+    this.counter++;
+    this.listDogs.push(newDog);
+    return newDog;
   }
 }
