60b891ba7d464085b62bd21c055f838e39ebaef9
Alex Charco Cuenca <alex.charco.25@gmail.com>
2023-02-14 12:58:02 -0500

    Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DogModule } from './dogs/dog.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [DogModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  //To use middleware it is required that the module class implements the NestModule interface
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('/dogs');
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '', method: RequestMethod.GET });
+  }
+}
