682767a3f1aea7df49db567244a61a54df37fc5c
Alex Charco Cuenca <alex.charco.25@gmail.com>
2023-02-15 10:38:41 -0500

    Middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { funtionLoggerMiddleware } from './middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Global Middleware
+  app.use(funtionLoggerMiddleware);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -4,7 +4,16 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request Logging: ', req.method, req.url);
+    console.log('Class Request Logging: ', req.method, req.url);
     next();
   }
 }
+
+export const funtionLoggerMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.log('Function Request Logging: ', req.method, req.url);
+  next();
+};
