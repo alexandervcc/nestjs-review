4b99e65be4a60bee36960eba24fd1b0ba8efbeb8
alexandervcc <alex.charco.25@gmail.com>
2023-02-28 17:06:27 -0500

    Query: Custom Args Class

--- graphql-proj/src/authors/author.resolver.ts
+++ graphql-proj/src/authors/author.resolver.ts
@@ -8,7 +8,9 @@ import {
 } from '@nestjs/graphql';
 import { PostService } from 'src/posts/post.service';
 import { AuthorService } from './author.service';
+import { GetAuthorArgs } from './dto/get-author.args';
 import { Author } from './models/author.model';
+import { Post } from './models/post.model';
 
 @Resolver((of) => Author)
 export class AuthorsResolver {
@@ -17,13 +19,18 @@ export class AuthorsResolver {
     private postsService: PostService,
   ) {}
 
-  @Query((returns) => Author)
-  async author(@Args('id', { type: () => Int }) id: number) {
+  @Query((returns) => Author, { name: 'author' })
+  async getAuthor(@Args('id', { type: () => Int }) id: number) {
     return this.authorsService.findOneById(id);
   }
 
-  @ResolveField()
-  async posts(@Parent() author: Author) {
+  @Query((returns) => Author, { name: 'author' })
+  async getAuthorDto(@Args() args: GetAuthorArgs) {
+    return this.authorsService.findOneByArgs(args);
+  }
+
+  @ResolveField('posts', (returns) => [Post])
+  async getPosts(@Parent() author: Author) {
     const { id } = author;
     return this.postsService.findAll({ authorId: id });
   }
--- graphql-proj/src/authors/author.service.ts
+++ graphql-proj/src/authors/author.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { GetAuthorArgs } from './dto/get-author.args';
 
 @Injectable()
 export class AuthorService {
   findOneById(id: number) {
+    return null;
+  }
+
+  findOneByArgs(args:GetAuthorArgs) {
     return null;
   }
 
