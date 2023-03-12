372ded9b3e21af2086d9a61b1bc45094912c33f0
alexandervcc <alex.charco.25@gmail.com>
2023-03-11 20:54:43 -0500

    DB Connection

---
+++ express-graphql/src/config/typeorm-mysql.ts
@@ -0,0 +1,21 @@
+import { DataSource } from "typeorm";
+import path from "path";
+
+export const connectDB = async () => {
+  const dbConnection = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "graphql",
+    password: "graphql",
+    database: "graphql",
+    entities: [path.join(__dirname, "../model/**.ts")],
+    synchronize: true,
+  });
+  try {
+    await dbConnection.initialize();
+    console.log("DB conected!");
+  } catch (error) {
+    console.error("Error connecting to the DB: ", error);
+  }
+};
--- express-graphql/src/index.ts
+++ express-graphql/src/index.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata";
 import { startServer } from "./app";
+import { connectDB } from "./config/typeorm-mysql";
 
 const SERVER_PORT = 3000;
 
 const main = async () => {
+  await connectDB();
   const app = await startServer();
   app.listen(SERVER_PORT);
   console.log("Server running on: ", SERVER_PORT);
